98d3ac028eb1e9fac3195ef1fe3d5ecfcf3f51b4
gh.matoo <matoogsm90@gmail.com>
2024-01-31 11:53:28 +0330

    creta base model features

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 import logging
 from functools import partial, update_wrapper
+from datetime import timedelta, datetime
 
 logger = logging.getLogger('schedule')
 
@@ -24,6 +25,15 @@ class Scheduler:
         job = Job(interval, self)
         return job
 
+    def run_pending(self):
+        runnable_jobs = (job for job in self.jobs if job.should_run)
+        for job in sorted(runnable_jobs):
+            self._run_job(job)
+
+    @staticmethod
+    def _run_job(job):
+        job.run()
+
 
 class Job:
 
@@ -31,7 +41,14 @@ class Job:
         self.interval = interval
         self.job_func = None
         self.unit = None
+        self.period = None
+        self.last_run = None
+        self.next_run = None
         self.scheduler = scheduler
+        self._unit_tuple = ('seconds', 'minutes', 'hours', 'days', 'weeks')
+
+    def __lt__(self, other):
+        return self.next_run < other.next_run
 
     @property
     def second(self):
@@ -43,23 +60,87 @@ class Job:
     def seconds(self):
         self.unit = 'seconds'
         return self
+    
+    @property
+    def minute(self):
+        if self.interval != 1:
+            raise IntervalError("Use seconds instead of minute")
+        return self.minute
+
+    @property
+    def minutes(self):
+        self.unit = 'minutes'
+        return self
+    
+    @property
+    def hour(self):
+        if self.interval != 1:
+            raise IntervalError("Use seconds instead of hour")
+        return self.hours
+
+    @property
+    def hours(self):
+        self.unit = 'hours'
+        return self
+
+    @property
+    def day(self):
+        if self.interval != 1:
+            raise IntervalError("Use seconds instead of day")
+        return self.days
+
+    @property
+    def days(self):
+        self.unit = 'days'
+        return self
+
+    @property
+    def week(self):
+        if self.interval != 1:
+            raise IntervalError("Use seconds instead of week")
+        return self.weeks
+
+    @property
+    def weeks(self):
+        self.unit = 'weeks'
+        return self
 
     def do(self, job_func, *arg, **kwargs):
         self.job_func = partial(job_func, *arg, **kwargs)
         update_wrapper(self.job_func, job_func)
-        self._scheduler_next_run()
         if self.job_func is None:
             raise SchedulerError("Unable to add job to scheduler ")
+        self._scheduler_next_run()
         self.scheduler.jobs.append(self)
         return self
 
+    def should_run(self):
+        assert self.next_run is not None, 'must run _scheduler_next_run before'
+        return self.next_run <= datetime.now()
+
+    def run(self):
+        logger.debug(f'Running job {self}')
+        ret = self.job_func()
+        self.last_run = datetime.now()
+        self._scheduler_next_run()
+        return ret
+
     def _scheduler_next_run(self):
-        pass
+        if self.unit not in self._unit_tuple:
+            raise SchedulerValueError(
+                f"Invalid unit (valid units are {self._unit_tuple})"
+            )
+        interval = self.interval
+        self.period = timedelta(**{self.unit: self.interval})
+        self.next_run = datetime.now() + self.period
 
 
 default_scheduler = Scheduler()
 
 
 def every(interval=1):
-    default_scheduler.every(interval)
+    return default_scheduler.every(interval)
 
+
+def run_pending():
+    return default_scheduler.run_pending()
