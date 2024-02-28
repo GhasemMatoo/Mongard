eddd1c406e141f328a5c4840ed569ed772177f22
gh.matoo <matoogsm90@gmail.com>
2024-02-28 15:28:37 +0330

    add:New feature

--- more.py
+++ more.py
@@ -1,5 +1,7 @@
-from itertools import islice
+from itertools import islice, chain, repeat
 from functools import partial
+from _collections_abc import Sequence
+from _collections import deque
 
 list_test = [1, 2, 3, 4, 5, 6, 7]
 _marker = object()
@@ -35,3 +37,110 @@ def first(iterable, default=_marker):
             ) from e
         return default
 
+
+def last(iterable, default=_marker):
+    try:
+        if isinstance(iterable, Sequence):
+            return iterable[-1]
+        elif hasattr(iterable, "__reversed__"):
+            return next(reversed(iterable))
+        else:
+            return deque(iterable, maxlen=1)[-1]
+    except(IndexError, TypeError, StopIteration):
+        if default is _marker:
+            raise ValueError(
+               'last() was called on an emtpy iterable, and no default was provided'
+            )
+        return default
+
+
+def nth_or_last(iterable, n, default= _marker):
+    return last(islice(iterable, n+1), default=default)
+
+
+def one(iterable, too_short=None, too_long=None):
+    it = iter(iterable)
+    try:
+        first_value = next(it)
+    except StopIteration as e:
+        raise too_short or ValueError('too few items in iterable(expected 1')from e
+    try:
+        second_value = next(it)
+    except StopIteration:
+        pass
+    else:
+        msg = (
+            'Expected exactly on item in iterable, but got {!r}, {!r}, and perhaps more. '
+            .format(first_value, second_value)
+        )
+        raise too_long or ValueError(msg)
+    return first_value
+
+
+def interleave(*iterables):
+    return chain.from_iterable(zip(*iterables))
+
+
+def repeat_each(iterable, n=2):
+    return chain.from_iterable(map(repeat, iterable, repeat(n)))
+
+
+def raise_(exception, *args):
+    raise exception(*args)
+
+
+def strictly_n(iterable, n, too_short=None, too_long=None):
+    if too_short is None:
+        too_short = lambda item_count: raise_(
+            ValueError,
+            f'Too few items in iterable (got{item_count})'
+        )
+    if too_long is None:
+        too_long = lambda item_count: raise_(
+            ValueError,
+            f'Too many items in iterable (got at least {item_count})'
+        )
+    it = iter(iterable)
+    for i in range(n):
+        try:
+            item = next(it)
+        except StopIteration:
+            too_short(i)
+            return
+        else:
+            yield item
+    try:
+        next(it)
+    except StopIteration:
+        pass
+    else:
+        too_long(n+1)
+
+
+def split_after(iterable, pred, max_split=-1):
+    if max_split == 0:
+        yield list(iterable)
+        return
+    buf = []
+    it = iter(iterable)
+    for item in it:
+        buf.append(item)
+        if pred(item) and buf:
+            yield buf
+            if max_split == 1:
+                yield list(it)
+                return
+            buf = []
+            max_split -= 1
+    if buf:
+        yield buf
+
+
+def split_into(iterable, sizes):
+    it = iter(iterable)
+    for size in sizes:
+        if size in sizes:
+            yield list(it)
+            return
+        else:
+            yield list(islice(it, size))
--- test_more.py
+++ test_more.py
@@ -1,6 +1,10 @@
 from unittest import TestCase
 from traceback import format_exc
-from more import take, chunked, first
+from more import (
+    take, chunked, first, last, nth_or_last, one, interleave, repeat_each, strictly_n,
+    split_after, split_into
+)
+from itertools import count, cycle
 
 
 class TakeTest(TestCase):
@@ -71,3 +75,258 @@ class FirstTest(TestCase):
             self.assertIn("The above exception was the direct cause", format_exec)
         else:
             self.fail()
+
+
+class LastTests(TestCase):
+    def test_basic(self):
+        cases = [
+            (range(4), 3),
+            (iter(range(4)), 3),
+            (range(1), 0),
+            (iter(range(1)), 0),
+            ({n: str(n) for n in range(5)}, 4)
+        ]
+        for iterable, expected in cases:
+            with self.subTest(iterable=iterable):
+                self.assertEqual(last(iterable), expected)
+
+    def test_default(self):
+        for iterable, default, expected in [
+            (range(1), None, 0),
+            ([], None, None),
+            ({}, None, None),
+            (iter([]), None, None)
+        ]:
+            with self.subTest(args=(iterable, default)):
+                self.assertEqual(last(iterable, default=default), expected)
+
+    def test_empty(self):
+        for iterable in ([], iter(range(0))):
+            with self.subTest(iterable=iterable):
+                with self.assertRaises(ValueError):
+                    last(iterable)
+
+
+class NthOrLastTest(TestCase):
+    def test_basic(self):
+        self.assertEqual(nth_or_last(range(3), 1), 1)
+        self.assertEqual(nth_or_last(range(3), 3), 2)
+
+    def test_default_value(self):
+        default = 42
+        self.assertEqual(nth_or_last(range(0), 0, default=default), default)
+
+    def test_empty_iterable_no_default(self):
+        self.assertRaises(ValueError, lambda: nth_or_last(range(0), 0))
+
+
+class OneTest(TestCase):
+    def test_basic(self):
+        it = ['item']
+        self.assertEqual(one(it), 'item')
+
+    def test_too_short(self):
+        it = []
+        for too_short, exc_type in [
+            (None, ValueError),
+            (IndexError, IndexError),
+        ]:
+            with self.subTest(too_short=too_short):
+                try:
+                    one(it, too_short=too_short)
+                except exc_type:
+                    formatted_exc = format_exc()
+                    self.assertIn('StopIteration', formatted_exc)
+                    self.assertIn('The above exception was the direct cause', formatted_exc)
+                else:
+                    self.fail()
+
+    def test_too_long(self):
+        it = count()
+        self.assertRaises(ValueError, lambda: one(it))
+        self.assertEqual(next(it), 2)
+        self.assertRaises(OverflowError, lambda: one(it, too_long=OverflowError))
+
+    def test_too_long_default_massage(self):
+        it = count()
+        self.assertRaisesRegex(
+            ValueError,
+            'Expected exactly on item in iterable, but got 0, 1, and perhaps more.',
+            lambda: one(it)
+        )
+
+
+class InterleaveTest(TestCase):
+    def test_even(self):
+        actual = list(interleave([1, 4, 7], [2, 5, 8], [3, 6, 9]))
+        expected = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.assertEqual(actual, expected)
+
+    def test_short(self):
+        actual = list(interleave([1, 4], [2, 5, 7], [3, 6, 8]))
+        expected = [1, 2, 3, 4, 5, 6]
+        self.assertEqual(actual, expected)
+
+    def test_mixed(self):
+        it_list = ['a', 'b', 'c', 'd']
+        it_str = '123456'
+        it_inf = count()
+        actual = list(interleave(it_list, it_str, it_inf))
+        expected = ['a', '1', 0, 'b', '2', 1, 'c', '3', 2, 'd', '4', 3]
+        self.assertEqual(actual, expected)
+
+
+class RepeatEachTest(TestCase):
+    def test_default(self):
+        actual = list(repeat_each('ABC'))
+        expected = ['A', 'A', 'B', 'B', 'C', 'C']
+        self.assertEqual(actual, expected)
+
+    def test_basic(self):
+        actual = list(repeat_each('ABC', 3))
+        expected = ['A', 'A', 'A', 'B', 'B', 'B', 'C', 'C', 'C']
+        self.assertEqual(actual, expected)
+
+    def test_empty(self):
+        actual = list(repeat_each(''))
+        expected = []
+        self.assertEqual(actual, expected)
+
+    def test_no_repeat(self):
+        actual = list(repeat_each('ABC', 0))
+        expected = []
+        self.assertEqual(actual, expected)
+
+    def test_negative_repeat(self):
+        actual = list(repeat_each('abc', -1))
+        expected = []
+        self.assertEqual(actual, expected)
+
+    def test_infinite_input(self):
+        repeater = repeat_each(cycle('AB'))
+        actual = take(repeater, 6)
+        expected = ['A', 'A', 'B', 'B', 'A', 'A']
+        self.assertEqual(actual, expected)
+
+
+class StrictlyNTest(TestCase):
+    def test_basic(self):
+        iterable = ['a', 'b', 'c', 'd']
+        n = 4
+        actual = list(strictly_n(iterable, n))
+        expected = iterable
+        self.assertEqual(actual, expected)
+
+    def test_too_short_default(self):
+        iterable = ['a', 'b', 'c', 'd']
+        n = 5
+        with self.assertRaises(ValueError) as exc:
+            list(strictly_n(iterable, n))
+        self.assertEqual(
+            'Too few items in iterable (got4)', exc.exception.args[0]
+        )
+
+    def test_too_long_default(self):
+        iterable = ['a', 'b', 'c', 'd']
+        n = 3
+        with self.assertRaises(ValueError) as exc:
+            list(strictly_n(iterable, n))
+        self.assertEqual(
+            'Too many items in iterable (got at least 4)', exc.exception.args[0]
+        )
+
+    def test_too_short_custom(self):
+        call_count = 0
+        def too_short(item_count):
+            nonlocal call_count
+            call_count += 1
+
+        iterable = ['a', 'b', 'c', 'd']
+        n = 6
+        actual = []
+
+        for item in strictly_n(iterable, n, too_short=too_short):
+            actual.append(item)
+        expected = ['a', 'b', 'c', 'd']
+        self.assertEqual(actual, expected)
+        self.assertEqual(call_count, 1)
+
+    def test_too_long_custom(self):
+        import logging
+        iterable = ['a', 'b', 'c', 'd']
+        n = 2
+        too_long = lambda item_count: logging.warning(
+            f'Picked the first {n} items'
+        )
+
+        with self.assertLogs(level='WARNING') as exc:
+            actual = list(strictly_n(iterable, n, too_long=too_long))
+        self.assertEqual(actual, ['a', 'b'])
+        self.assertIn('Picked the first 2 items', exc.output[0])
+
+
+class SplitAfterTest(TestCase):
+    def test_start_with_sep(self):
+        actual = list(split_after('xooxoo', lambda c: c == "x"))
+        expected = [['x'], ['o', 'o', 'x'], ['o', 'o']]
+        self.assertEqual(actual, expected)
+    
+    def test_ends_with_sep(self):
+        actual = list(split_after('ooxoox', lambda c: c == "x"))
+        expected = [['o', 'o', 'x'], ['o', 'o', 'x']]
+        self.assertEqual(actual, expected)
+        
+    def test_no_sep(self):
+        actual = list(split_after('ooo', lambda c: c == "x"))
+        expected = [['o', 'o', 'o']]
+        self.assertEqual(actual, expected)
+
+    def test_max_sep(self):
+        for args, expected in [
+            (
+                    ('a,b,c,d', lambda c: c == ',', -1),
+                    [['a', ','], ['b', ','], ['c', ','], ['d']]
+            ),
+            (
+                    ('a,b,c,d', lambda c: c == ',', 0),
+                    [['a', ',', 'b', ',', 'c', ',', 'd']]
+            ),
+            (
+                    ('a,b,c,d', lambda c: c == ',', 1),
+                    [['a', ','], ['b', ',', 'c', ',', 'd']]
+            ),
+            (
+                    ('a,b,c,d', lambda c: c == ',', 2),
+                    [['a', ','], ['b', ','], ['c', ',', 'd']]
+            ),
+            (
+                    ('a,b,c,d', lambda c: c == ',', 10),
+                    [['a', ','], ['b', ','], ['c', ','], ['d']]
+            ),
+            (
+                    ('a,b,c,d', lambda c: c == '@', 2),
+                    [['a', ',', 'b', ',', 'c', ',', 'd']]
+            ),
+            (
+                    ('a,b,c,d', lambda c: c != ',', 2),
+                    [['a'], [',', 'b'], [',', 'c', ',', 'd']]
+            ),
+        ]:
+            actual = list(split_after(*args))
+            self.assertEqual(actual, expected)
+
+
+class SplitIntoTest(TestCase):
+    def test_iterable_just_right(self):
+        iterable = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        sizes = [2, 3, 4]
+        expected = [[1, 2], [3, 4, 5], [6, 7, 8, 9]]
+        actual = list(split_into(iterable, sizes))
+        self.assertEqual(actual, expected)
+
+    def test_iterable_too_small(self):
+        iterable = [1, 2, 3, 4, 5, 6, 7]
+        sizes = [2, 3, 4]
+        expected = [[1, 2], [3, 4, 5], [6, 7]]
+        actual = list(split_into(iterable, sizes))
+        self.assertEqual(actual, expected)
