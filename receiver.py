1252a4aa02e4a5ca47854cba19434b3f4e8a3098
gh.matoo <matoogsm90@gmail.com>
2024-07-06 12:11:44 +0330

    rabbitmq use prefetch_count and delivery_tag in basic_ack

--- receiver.py
+++ receiver.py
@@ -1,7 +1,7 @@
 import pika
 
 credentials = pika.PlainCredentials(username="ghasem", password="Mg1368")
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', credentials=credentials))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1', credentials=credentials))
 
 chanel_1 = connection.channel()
 chanel_1.queue_declare(queue="one")
@@ -9,9 +9,12 @@ chanel_1.queue_declare(queue="one")
 
 def callback(ch, method, properties, body):
     print(f'booody{body}')
+    print(f'method{method}')
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
-chanel_1.basic_consume(queue="one", on_message_callback=callback, auto_ack=True)
+chanel_1.basic_qos(prefetch_count=1)
+chanel_1.basic_consume(queue="one", on_message_callback=callback)
 print('Waiting for receiving message, to exit press ctrl+c')
 
 if input() == 'exit':
--- sender.py
+++ sender.py
@@ -1,11 +1,12 @@
 import pika
 
 credentials = pika.PlainCredentials(username="ghasem", password="Mg1368")
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost', credentials=credentials))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1', credentials=credentials))
 chanel_1 = connection.channel()
 
 chanel_1.queue_declare(queue="one")
 
-chanel_1.basic_publish(exchange="", routing_key="one", body="Hello world...!")
+properties = pika.BasicProperties(headers={"test": 'TEST'})
+chanel_1.basic_publish(exchange="", routing_key="one", body="Hello world...!", properties=properties)
 print("Message sending")
 chanel_1.close()
