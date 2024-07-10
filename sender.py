a07b809ebb39fbfd9e81f7271c2974ee3dd0766e
gh.matoo <matoogsm90@gmail.com>
2024-07-10 11:12:37 +0330

    rabbitmq use ex to ex

--- sender.py
+++ sender.py
@@ -1,23 +1,13 @@
 import pika
-from pika.exchange_type import ExchangeType
-from uuid import uuid4
+
 credentials = pika.PlainCredentials(username="ghasem", password="Mg1368")
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='127.0.0.1', credentials=credentials))
 chanel_1 = connection.channel()
 
-reply_queue = chanel_1.queue_declare(queue='', exclusive=True)
+chanel_1.exchange_declare(exchange="first", exchange_type="direct")
+chanel_1.exchange_declare(exchange="second", exchange_type="fanout")
+chanel_1.exchange_bind("first", "second")
 
-
-def on_reply_message_receive(ch, method, properties, body):
-    print(f'reply_message_receive == {body}')
-    ch.basic_ack(delivery_tag=method.delivery_tag)
-
-chanel_1.basic_consume(queue=reply_queue.method.queue, on_message_callback=on_reply_message_receive)
-
-chanel_1.queue_declare(queue="request-queue")
-core_id = str(uuid4())
-messages = "Hello Word...!"
-properties = pika.BasicProperties(reply_to=reply_queue.method.queue, correlation_id=core_id)
-chanel_1.basic_publish(exchange="", routing_key="request-queue", body=messages, properties=properties)
-print("Message sending")
-chanel_1.start_consuming()
+chanel_1.basic_publish(exchange="firest", routing_key="", body="hello .....!")
+print("Sending Message ......")
+connection.close()
