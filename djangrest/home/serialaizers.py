1514f62e7e3da6aba29a93b812c45a934e8a5e25
matoo <matoogsm90@gmail.com>
2025-01-20 23:26:46 +0330

    ADD:QuestionSerializer and AnswerSerializer

--- djangrest/home/serialaizers.py
+++ djangrest/home/serialaizers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from .models import Question, Answer
 
 
 class PersonSerializer(serializers.Serializer):
@@ -6,3 +7,20 @@ class PersonSerializer(serializers.Serializer):
     age = serializers.IntegerField()
     email = serializers.EmailField()
 
+
+class QuestionSerializer(serializers.ModelSerializer):
+    answers = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+    def get_answers(self, obj):
+        result = obj.qw_answers.all()
+        return AnswerSerializer(instance=result, many=True).data
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = '__all__'
--- djangrest/home/urls.py
+++ djangrest/home/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 app_name = 'home'
 urlpatterns = [
-    path('', views.HomeView.as_view(), name="home")
+    path('', views.HomeView.as_view(), name="home"),
+    path('questions/', views.QuestionListView.as_view(), name="questions"),
+    path('question/create/', views.QuestionCreateView.as_view(), name="questions_create"),
+    path('question/update/<int:pk>/', views.QuestionUpdateView.as_view(), name="questions_update"),
+    path('question/delete/<int:pk>/', views.QuestionDeleteView.as_view(), name="questions_delete"),
 ]
