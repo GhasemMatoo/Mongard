bd526b80e04f1b6722abc74a9ebd5705e8e7c593
matoo <as.matoo1401@gmail.com>
2025-02-15 14:35:59 +0330

    ADD: TestWriterView and client login

--- djangotest/home/tests/test_views.py
+++ djangotest/home/tests/test_views.py
@@ -34,11 +34,29 @@ class TestUserRegisterViews(TestCase):
             reverse('home:user_register'),
             data={
                 'user_name': 'ghasem',
-                'user_email': 'matoogsm90@gma',
+                'user_email': 'invalid_email',
                 'user_password': 'matoo',
                 'user_re_password': 'matoo',
             }
         )
+        form = response.context['form']({
+            'user_name': 'ghasem',
+            'user_email': 'invalid_email',
+            'user_password': 'matoo',
+            'user_re_password': 'matoo',
+        })
         self.assertEqual(response.status_code, 200)
-        self.failIf(response.context['form'].is_valid())
+        self.assertFalse(form.is_valid())
+        self.assertFormError(form=form, field='user_email', errors="Enter a valid email address.")
 
+
+class TestWriterView(TestCase):
+    def setUp(self) -> None:
+        User.objects.create_user(username='matoo', email='matoogsm90@gmail.com', password='ghasem')
+        self.client = Client()
+        self.client.login(username='matoo', password='ghasem')
+
+    def test_writer_GET(self):
+        response = self.client.get(reverse('home:writer'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, WriterView.template_name)
