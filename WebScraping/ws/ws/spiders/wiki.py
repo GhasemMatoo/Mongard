bc66b006e591d8554a7c9e0c4562b909ff0988d7
matoo <as.matoo1401@gmail.com>
2025-07-15 13:37:42 +0330

    ADD:get data css and xpath

--- WebScraping/ws/ws/spiders/wiki.py
+++ WebScraping/ws/ws/spiders/wiki.py
@@ -7,9 +7,17 @@ class WikiSpider(scrapy.Spider):
     start_urls = [
         "https://en.wikipedia.org/wiki/Bruce_Lee",
         "https://en.wikipedia.org/wiki/Carl_Sagan",
+        "https://en.wikipedia.org/wiki/Marie_Curie"
     ]
 
     def parse(self, response, **kwargs):
-        title = response.css('title').extract()
-        yield {'title': title}
-
+        # title = response.css('title').extract()
+        title_css = response.css('title::text').get()
+        # title = response.xpath('/html/head/title').get()
+        title = response.xpath('/html/head/title/text()').get()
+        role = response.css('div#p-vector-user-menu-overflow::attr(class)').get()
+        yield {
+            'title_css': title_css,
+            'title_xpath': title,
+            'attr': role
+        }
