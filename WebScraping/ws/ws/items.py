9457cebe6ce40e2d8b7391dd38f472b6753073c1
matoo <as.matoo1401@gmail.com>
2025-07-16 10:46:44 +0330

    ADD:Items and ItemsLoader

--- WebScraping/ws/ws/items.py
+++ WebScraping/ws/ws/items.py
@@ -4,9 +4,26 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from w3lib.html import remove_tags
+from itemloaders.processors import TakeFirst, MapCompose
+
+
+def to_strip(value):
+    return value.strip()
+
+
+def to_uppercase(value):
+    return value.upper()
 
 
 class WsItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+class CountryItem(scrapy.Item):
+    name = scrapy.Field(
+        input_processor=MapCompose(remove_tags, to_strip, to_uppercase), output_processor=TakeFirst())
+    capital = scrapy.Field(output_processor=TakeFirst())
+    population = scrapy.Field(output_processor=TakeFirst())
