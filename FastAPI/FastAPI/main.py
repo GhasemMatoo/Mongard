d4cd1427594aac577393ed9bbcea71ee33049166
matoo <as.matoo1401@gmail.com>
2025-09-17 12:20:09 +0330

    ADD:request body by pydantic

--- FastAPI/FastAPI/main.py
+++ FastAPI/FastAPI/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from typing import Optional, Union
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Person(BaseModel):
+    name: str
+    age: int | None = 0  # optional by python 3.10
+    height: Optional[int] = 0  # optional by python 3.6
+    weight: Union[int, float] = None
+
+
+@app.post("/person/")
+async def reade_person(person: Person) -> Person:
+    return person
 
 
 @app.get("/")
