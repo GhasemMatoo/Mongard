0e9a4d3be4bd159befc26b56ad64f70037038a9f
matoo <as.matoo1401@gmail.com>
2025-09-17 12:01:43 +0330

    ADD:path parameter and query parameter

--- FastAPI/FastAPI/main.py
+++ FastAPI/FastAPI/main.py
@@ -4,20 +4,24 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+def read_root() -> dict:
     return {"message": "Hello World"}
 
 
 @app.get("/hello/{name}")
-def read_hello(name: str):
-    return {"message": f"Hello, {name}!"}
+def read_hello(name: str, age: int = 0) -> dict:
+    # name == Path Parameter
+    # age == Query Parameter
+    return {"message": f"Hi {name}, you are {age} years"}
 
 
 @app.get("/async/")
-async def root():
+async def root() -> dict:
     return {"message": "Hello World"}
 
 
 @app.get("/async/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+async def say_hello(name: str, age: int = 0) -> dict:
+    # name == Path Parameter
+    # age == Query Parameter
+    return {"message": f"Hi {name}, you are {age} years"}
