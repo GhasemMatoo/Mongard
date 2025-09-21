2b829f3081cc83916fe14c4dba2ae84afad45b2b
matoo <as.matoo1401@gmail.com>
2025-09-21 10:34:13 +0330

    ADD:on_event

--- FastAPI/FastAPI/main.py
+++ FastAPI/FastAPI/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routers import user
+from datetime import datetime
 import time
 
 app = FastAPI()
@@ -13,3 +14,15 @@ async def add_process_time_header(request, call_next):
     process_time = time.time() - start_time
     response.headers['X-Process-Time'] = str(process_time)
     return response
+
+
+@app.on_event('startup')
+async def startup_event():
+    with open('server_time_log.log', 'a') as log:
+        log.write(f'Server started at:{datetime.now()} \n')
+
+
+@app.on_event('shutdown')
+async def shutdown_event():
+    with open('server_time_log.log', 'a') as log:
+        log.write(f'Server shut down at:{datetime.now()} \n')
