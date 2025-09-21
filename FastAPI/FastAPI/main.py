4653493db9aaf33727b5da4328ac0e56faef6b2b
matoo <as.matoo1401@gmail.com>
2025-09-21 10:25:24 +0330

    ADD:middleware

--- FastAPI/FastAPI/main.py
+++ FastAPI/FastAPI/main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 from routers import user
+import time
 
 app = FastAPI()
-app.include_router(user.router)
+app.include_router(user.router, tags=["all"])
+
+
+@app.middleware('http')
+async def add_process_time_header(request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers['X-Process-Time'] = str(process_time)
+    return response
